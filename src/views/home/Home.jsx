eaed055f98ad0974f208d15a76019e7268d29de9
rahulspace <rahultp518@gmail.com>
2020-12-06 19:30:26 +0530

    Create home view

---
+++ src/views/home/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import styles from './Home.module.scss'
+
+export default class Home extends Component {
+  render () {
+    return (
+      <div className={styles.home}>home</div>
+    )
+  }
+}
